20542218bc0d7c2fe06c461b0c79569c72257706
ioanarijnita <ioanarijnita@gmail.com>
2021-04-29 23:47:49 +0300

    implemented As a Client or Bank Representative, I want to be able to register task - SB20

--- src/main/java/smart/banking/Main.java
+++ src/main/java/smart/banking/Main.java
@@ -1,21 +1,37 @@
 package smart.banking;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Label;
 import javafx.stage.Stage;
+import smart.banking.services.FileSystemService;
+import smart.banking.services.UserService;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
 
 public class Main extends Application {
 
-    public static void main(String[] args) {
-        launch(args);
-    }
 
     @Override
-    public void start(Stage primaryStage) {
-        Label label = new Label("Hello World");
-        Scene scene = new Scene(label);
-        primaryStage.setScene(scene);
+    public void start(Stage primaryStage) throws Exception {
+        initDirectory();
+        UserService.initDatabase();
+        Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("register.fxml"));
+        primaryStage.setTitle("Smart-Banking");
+        primaryStage.setScene(new Scene(root, 360, 525));
         primaryStage.show();
+    }
+
+    private void initDirectory() {
+        Path applicationHomePath = FileSystemService.APPLICATION_HOME_PATH;
+        if (!Files.exists(applicationHomePath))
+            applicationHomePath.toFile().mkdirs();
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
     }
 }
