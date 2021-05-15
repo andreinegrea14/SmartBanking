7179203a04a20f5594c64415f18ffdc243332fcb
ioanarijnita <ioanarijnita@gmail.com>
2021-05-16 00:45:42 +0300

    changed client & bank representative menus UI/SB-29

--- src/main/java/smart/banking/controllers/RequestBankSupportController.java
+++ src/main/java/smart/banking/controllers/RequestBankSupportController.java
@@ -50,4 +50,43 @@ public class RequestBankSupportController {
         window.show();
     }
 
+    public void handleLogout(ActionEvent event) throws IOException {
+        Parent loginWindow = FXMLLoader.load(CheckBalanceController.class.getResource("/login.fxml"));
+        Scene loginScene = new Scene(loginWindow,600, 525);
+
+        Stage window =  ((Stage) (((Node) event.getSource()).getScene().getWindow()));
+        window.setScene(loginScene);
+        window.show();
+
+    }
+
+    public void openCheckBalance(ActionEvent event) throws IOException {
+        Parent adminWindow = FXMLLoader.load(ClientController.class.getResource("/checkbalance.fxml"));
+        Scene adminScene = new Scene(adminWindow);
+        Stage window =  ((Stage) (((Node) event.getSource()).getScene().getWindow()));
+        window.setScene(adminScene);
+        window.setTitle("Check Balance");
+        window.show();
+    }
+
+    public void transferFundsOnPress(ActionEvent event) throws IOException {
+        Parent adminWindow = FXMLLoader.load(ClientController.class.getResource("/transferFunds.fxml"));
+        Scene adminScene = new Scene(adminWindow);
+        Stage window =  ((Stage) (((Node) event.getSource()).getScene().getWindow()));
+        window.setScene(adminScene);
+        window.setTitle("Transfer Funds");
+        window.show();
+
+    }
+
+    public void seeTransactions(ActionEvent event) throws IOException {
+        Parent adminWindow = FXMLLoader.load(ClientController.class.getResource("/seeTransactions.fxml"));
+        Scene adminScene = new Scene(adminWindow);
+        Stage window =  ((Stage) (((Node) event.getSource()).getScene().getWindow()));
+        window.setScene(adminScene);
+        window.setTitle("List of all transactions");
+        window.show();
+
+    }
+
 }
